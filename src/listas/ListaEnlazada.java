ec1205b48aa928d4114a19de774624a855767e9b
sirlocust <jua56yu@hotmail.com>
2020-08-22 02:46:21 -0500

    creando los metodos de la lista enlazada

--- src/listas/ListaEnlazada.java
+++ src/listas/ListaEnlazada.java
@@ -1,5 +1,98 @@
 package listas;
 
 public class ListaEnlazada {
+        protected  Nodo primero;
+
+
+        public ListaEnlazada(){
+            primero = null;
+        }
+
+        public ListaEnlazada agregarI(int dato){
+            Nodo nuevo;
+            nuevo = new Nodo(dato);
+            nuevo.ref = this.primero;
+            this.primero = nuevo;
+            return this;
+
+
+        }
+
+        public void agregar(int dato){
+            Nodo nuevo = new Nodo(dato);
+//            if(primero.getRef() == null){
+//                primero.setRef(nuevo);
+//                nuevo.setRef(null);
+//            }
+            Nodo last = primero;
+            while (last != null){
+                if(last.ref == null){
+                    last.setRef(nuevo);
+                    break;
+//                  nuevo.setRef(null);
+                }else{
+
+                    last = last.ref;
+                }
+            }
+
+        }
+        public void eliminar(int dato){
+            Nodo delete = this.buscar(dato);
+            Nodo before = primero;
+            while (before != null){
+                if(before.getRef().equals(delete)){
+                    before.setRef(delete.getRef());
+                    delete.setRef(null);
+                    break;
+                }
+                before  = before.getRef();
+            }
+
+        }
+
+
+        public Nodo buscar(int d){
+            Nodo last = primero;
+            while (last != null){
+                if(last.getDato() == d){
+//                    System.out.println(last.getDato());
+                    return last;
+//                  nuevo.setRef(null);
+                }else{
+
+                    last = last.ref;
+                }
+            }
+            return last;
+        }
+
+        public void ver(){
+            Nodo nodo;
+
+            nodo = primero;
+            while (nodo != null){
+                System.out.println(nodo.dato);
+                nodo = nodo.ref;
+
+            }
+        }
+
+    public static void main(String[] args) {
+        ListaEnlazada  lista = new ListaEnlazada();
+        lista.agregarI(3);
+        lista.agregar(4);
+        lista.agregar(5);
+        lista.agregar(13);
+        lista.agregar(50);
+        lista.eliminar(5);
+        System.out.println(lista.buscar(5));
+        lista.ver();
+
+    }
+
+
+
+
 
 }
--- src/listas/Nodo.java
+++ src/listas/Nodo.java
@@ -9,4 +9,22 @@ public class Nodo {
         this.ref = null;
     }
 
+    public int getDato(){
+        return this.dato;
+    }
+
+    public void setRef(Nodo ref){
+        this.ref = ref;
+    }
+
+    public Nodo getRef(){
+        return this.ref;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "dato=" + dato +
+                '}';
+    }
 }
