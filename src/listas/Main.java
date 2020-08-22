44c5c7715f569b61ee3bad357d36d18728dfa7af
sirlocust <jua56yu@hotmail.com>
2020-08-22 02:45:36 -0500

    lista enlazada teoria

---
+++ src/listas/Main.java
@@ -0,0 +1,8 @@
+package listas;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/listas/Nodo.java
@@ -0,0 +1,12 @@
+package listas;
+
+public class Nodo {
+    int dato;
+    Nodo ref;
+
+    public Nodo(int dato){
+        this.dato = dato;
+        this.ref = null;
+    }
+
+}
